006318ba160b31adb8de4da6ee9b2d8edd13b1b4
wang seong hyeok <wang7807@naver.com>
2022-08-26 13:59:16 +0900

    save

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
+const axios = require("axios");
 const cors = require("cors");
 const app = express();
 app.set("port", process.env.PORT || 8100);
@@ -8,6 +11,7 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("hello express");
 });
+
 app.get("/megabox", async (req, res) => {
   // const item = req.params.item;
   // const searchItem = encodeURIComponent(item);
